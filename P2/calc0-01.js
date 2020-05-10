f5aba9ed2d7373494714cd62c89a17f6f901f72e
Alex <a.relano@alumnos.urjc.es>
2020-05-10 20:50:43 +0200

    funcionamiento basico sin maquina de estados

--- P2/calc0-01.js
+++ P2/calc0-01.js
@@ -5,6 +5,7 @@ display = document.getElementById("display")
 //boton2 = document.getElementById("boton2")
 suma = document.getElementById("suma")
 igual = document.getElementById("igual")
+del = document.getElementById("del")
 clear = document.getElementById("clear")
 
 digito = document.getElementsByClassName("digito")
@@ -18,15 +19,25 @@ for (i=0; i<digito.length; i++) {
 }
 
 //-- Insertar operadores
-suma.onclick = (ev) => {
-  display.innerHTML += ev.target.value;
+for (i=0; i<operador.length; i++) {
+  operador[i].onclick = (ev) => {
+    display.innerHTML += ev.target.value;
+  }
 }
-
 //-- Evaluar la expresion
 igual.onclick = () => {
   display.innerHTML = eval(display.innerHTML);
+  display.innerHTML = display.innerHTML;
 }
+//-- Borrar ultima cifra
+del.onclick = () => {
+  console.log("click en del");
+  B = display.innerHTML.length - 1;
+//  console.log(display.innerHTML.length);
+//  console.log(B);
+  display.innerHTML = display.innerHTML.slice(0,B);
 
+}
 //-- Poner a cero la expresion
 clear.onclick = () => {
   display.innerHTML = "0";
