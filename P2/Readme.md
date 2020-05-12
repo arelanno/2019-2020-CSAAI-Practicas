f9997e9ff301c5501586f464bbcaacc3e4803e21
Alex <a.relano@alumnos.urjc.es>
2020-05-12 03:32:18 +0200

    ultimas mejoras de diseño (fuentes, sonido etc) interruptor, operaciones

--- P2/Readme.md
+++ P2/Readme.md
@@ -1,1 +1,4 @@
 # Práctica 2
+Calculadora estilo retro.
+Todos los botones de la calculadora son funcionales pero hay que encenderla para que funcione.
+Para una experiencia total estilo años 80, habilitar el sonido.
--- P2/calc0-01.js
+++ P2/calc0-01.js
@@ -11,61 +11,86 @@ power= document.getElementById("power")
 
 digito = document.getElementsByClassName("digito")
 operador = document.getElementsByClassName("operador")
-Variable_calculo = 0;
+useless = document.getElementsByClassName("pointless")
+var audioerror = document.getElementById("audioerror");
+var audiogeneral = document.getElementById("audiogeneral");
+
 PW = false
 
 power.onclick = () => {
+  audiogeneral.play();
   if (PW == false) {
     PW = true;
     document.body.style.backgroundImage = "url('calculadoraresizedON.jpg')";
+        display.innerHTML = "0";
   }else{
     PW = false;
     document.body.style.backgroundImage = "url('calculadoraresizedOFF.jpg')";
+      display.innerHTML = ".";
   }
 }
 // -- Insertar digitos
 for (i=0; i<digito.length; i++) {
   digito[i].onclick = (ev) => {
-    if (display.innerHTML.length == 1 && display.innerHTML == 0 ) {
+    audiogeneral.play();
+    if (PW) {
+    if (display.innerHTML.length == 1 && display.innerHTML == 0) {
       display.innerHTML = ev.target.value;
     }else{
     display.innerHTML += ev.target.value;
-    Variable_calculo = display.innerHTML;
     }
-  }
+  } }
 }
 
 //-- Insertar operadores
 for (i=0; i<operador.length; i++) {
   operador[i].onclick = (ev) => {
+    audiogeneral.play();
+    if (PW) {
     display.innerHTML += ev.target.value;
-    Variable_calculo = display.innerHTML;
-  }
+  }}
 }
 
 sqrt.onclick = () => {
+  audiogeneral.play();
   console.log("click en RAIZ");
+  if (PW) {
   display.innerHTML = Math.sqrt(display.innerHTML);
-}
+}}
 
 //-- Borrar ultima cifra
 del.onclick = () => {
+  audiogeneral.play();
   console.log("click en del");
+  if (PW) {
   B = display.innerHTML.length - 1;
 /*  console.log(display.innerHTML.length);
  console.log(B);*/
   display.innerHTML = display.innerHTML.slice(0,B);
   if (display.innerHTML.length == 0) {
     display.innerHTML = "0";
-  }
+  }}
 }
 
 //-- Evaluar la expresion
 igual.onclick = () => {
+  audiogeneral.play();
+  if (PW) {
   display.innerHTML = eval(display.innerHTML);
-}
+}}
 
 //-- Poner a cero la expresion
 clear.onclick = () => {
+  audiogeneral.play();
+  if (PW) {
   display.innerHTML = "0";
-}
+}}
+
+for (i=0; i<useless.length; i++) {
+  useless[i].onclick = (ev) => {
+   if (PW) {
+     audioerror.play();
+   }else {
+     audiogeneral.play();
+   }
+} }
