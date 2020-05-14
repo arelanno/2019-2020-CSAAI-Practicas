e6a17eadc53236e0f5768dad289e733b898315cd
Alex <a.relano@alumnos.urjc.es>
2020-05-14 02:44:32 +0200

    pruebas

--- P3/L7/canvas-01.js
+++ P3/L7/canvas-01.js
@@ -7,28 +7,25 @@ canvas.height = 200;
 
 const ctx = canvas.getContext("2d");
 
-ctx.rect(10,10, 100, 50);
-
-ctx.strokeStyle = 'red';
-ctx.fillStyle = 'red';
-ctx.lineWidth = 20;
+ctx.beginPath();
+//-- Dibujar un circulo: coordenadas x,y del centro
+//-- Radio, Angulo inicial y angulo final
+ctx.arc(100, 50, 40, 0, 2 * Math.PI);
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 3;
+ctx.fillStyle = 'yellow';
 ctx.stroke()
-  //ctx.fill()
 
-  const linea = canvas.getContext("2d");
+//-- Dibujar el relleno
+ctx.fill()
 
-  //-- Línea horizontal
-  linea.moveTo(100, 100);
-  linea.lineTo(200, 200);
 
-  //-- Línea horizontal y vertical, unidas
-  linea.moveTo(10, 80);
-  linea.lineTo(150,80);
-  linea.lineTo(150,20);
+//-- Texto solido
+ctx.font = "25px Arial";
+ctx.fillStyle = 'grey'
+ctx.fillText("Texo sólido", 10, 30);
 
-  linea.strokeStyle = 'green';
-  //-- Cambiar el tamaño de la linea del trazo
-  linea.lineWidth = 4;
-
-  //-- Dibujar el trazo
-  linea.stroke()
+//-- Texto trazo
+ctx.strokeStyle = 'red';
+ctx.font = "35px Arial";
+ctx.strokeText("Texto trazo", 5, 180);//-- Dibujar el trazo
