49c5491bba9a0566af71ca85056e4fedf190a947
Alex <a.relano@alumnos.urjc.es>
2020-05-17 01:48:51 +0200

    creada clase bola

--- P3/pong-01.js
+++ P3/pong-01.js
@@ -11,51 +11,22 @@ console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
 //-- Obtener el contexto para pintar en el canvas
 const ctx = canvas.getContext("2d");
 
-//-- Objeto: Bola
-const bola = {
-
-  //-- Constante: Tamaño de la bola
-  size : 5,
-  //-- Contante: Posicion inicial de la bola
-  x_ini : 100,
-  y_ini : 200,
-  //-- Posicion generica de la bola
-  x : 0,
-  y : 0,
-  //-- Velocidad inicial de la bola
-  vx_ini : 6,
-  vy_ini : 3,
-  //-- Velocidad genérica de la bola
-  //-- Inicialmente a cero
-  vx : 0,
-  vy : 0,
-}
 
 //-- Objeto raqueta
 const raqI = {
   //-- Constante: Tamaño de la raqueta
   width : 10,
-  height: 40,
+  height: 50,
   //-- Constante: Posicion inicial
   x_ini : 50,
   y_ini : 100,
   //-- Constante: Velocidad
-  v_ini : 3,
+  v_ini : 4,
   //-- Velocidad (variable)
   v : 0,
 }
-//-- Pintar todos los objetos en el canvas
-function bola_draw() {
 
-  //----- Dibujar la Bola
-  ctx.beginPath();
-  ctx.fillStyle='white';
-
-  //-- x,y, anchura, altura
-  ctx.rect(bola.x, bola.y, bola.size, bola.size);
-  ctx.fill();
-}
-
+//Funciones de la raqueta
 function raqI_init(){
   raqI.x = raqI.x_ini;
   raqI.y = raqI.y_ini;
@@ -74,11 +45,16 @@ function raqI_draw()
   ctx.fill();
 }
 
+function raqI_update()
+{
+  raqI.y += raqI.v;
+}
+
 //-- Pintar todos los objetos en el canvas
 function draw() {
 
   //----- Dibujar la Bola
-  bola_draw();
+  bola.draw();
 
   //-- Dibunar la raqueta izquierda
   raqI_draw();
@@ -119,27 +95,28 @@ function draw() {
 //---- Bucle principal de la animación
 function animacion()
 {
+  //-- Actualizar la raqueta con la velocidad actual
+  raqI_update();
+
   if (bola.x >= canvas.width || bola.x <= 0 ) {
-    //-- Hay colisión. Cambiar el signo de la bola
+    //-- Hay colisión. Cambiar el signo de la bola en X
     bola.vx = bola.vx * -1;
   }
 
   if (bola.y >= canvas.height || bola.y <= 0 ) {
-    //-- Hay colisión. Cambiar el signo de la bola
+    //-- Hay colisión. Cambiar el signo de la bola en Y
     bola.vy = bola.vy * -1;
   }
 
   //-- Comprobar si hay colisión con la raqueta izquierda
-  if (bola.x >= raqI.x && bola.x <=(raqI.x+10) &&
-      bola.y >= raqI.y && bola.y <=(raqI.y+40)) {
+  if (bola.x >= raqI.x && bola.x <=(raqI.x+raqI.width) &&
+      bola.y >= raqI.y && bola.y <=(raqI.y+raqI.height)) {
     bola.vx = bola.vx * -1;
   }
   //-- Actualizar las posiciones de los objetos móviles
     //-- Actualizar coordenada x de la bola
-    bola.x += bola.vx;
-    bola.y += bola.vy;
+  bola.update();
 
-    raqI.y += raqI.v;
   //-- Borrar la pantalla
   ctx.clearRect(0,0, canvas.width, canvas.height);
 
@@ -147,13 +124,12 @@ function animacion()
   draw();
 }
 
-//-- Inicializa la bola: A su posicion inicial
-bola.x = bola.x_ini;
-bola.y = bola.y_ini;
+//-- Inicializa la bola: Llevarla a su posicion inicial
+const bola = new Bola(ctx);
+bola.init();
 
 //-- Inicializar la raqueta a su posicion inicial
-raqI.x = raqI.x_ini;
-raqI.y = raqI.y_ini;
+raqI_init();
 
 //-- Arrancar la animación
 setInterval(()=>{
@@ -175,9 +151,9 @@ window.onkeydown = (e) => {
       break;
     //-- Tecla ESPACIO: Saque
     case " ":
-      bola.x = bola.x_ini;
+      bola.init();
+      //-- Se le da velocidad
       bola.vx = bola.vx_ini;
-      bola.y = bola.y_ini;
       bola.vy = bola.vy_ini;
   console.log("saque!");
   }
